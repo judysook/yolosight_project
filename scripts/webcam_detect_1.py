1edc81acda7a7583e1a8e39b8b96b7e94e97b5df
seojin031 <gin031@naver.com>
2025-06-17 21:52:43 +0900

    Add files via upload

---
+++ scripts/webcam_detect_1.py
@@ -0,0 +1,97 @@
+import numpy as np
+import cv2
+
+def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=False,
+              scaleFill=False, scaleup=True, stride=32):
+    
+    shape = im.shape[:2]  
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:
+        r = min(r, 1.0)
+
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]
+    if auto:
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)
+
+    dw /= 2
+    dh /= 2
+
+    if shape[::-1] != new_unpad:
+        im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
+
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
+
+    return im, r, (dw, dh)
+
+import torch
+
+def scale_coords(img1_shape, coords, img0_shape, ratio_pad=None):
+    if ratio_pad is None:  
+        gain = min(img1_shape[0] / img0_shape[0], img1_shape[1] / img0_shape[1])  
+        pad = ((img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2)  
+    else:
+        gain = ratio_pad[0][0]
+        pad = ratio_pad[1]
+
+    coords[:, [0, 2]] -= pad[0]  
+    coords[:, [1, 3]] -= pad[1]  
+    coords[:, :4] /= gain
+    coords[:, :4] = coords[:, :4].clamp(min=0)
+    return coords
+
+from pathlib import Path
+from models.common import DetectMultiBackend  
+from utils.general import non_max_suppression
+
+
+weights = 'best_cpu_win.pt'
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = DetectMultiBackend(weights, device=device)
+print(model.names) # 전체 리스트 목록 출력용 ( tts 구현시 참고 ! )
+stride = model.stride
+names = model.names
+
+
+cap = cv2.VideoCapture(0)
+assert cap.isOpened(), '웹캠을 열 수 없습니다.'
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    
+    img = letterbox(frame, 640, stride=stride)[0]
+    img = img.transpose((2, 0, 1))[::-1].copy()  
+    img = torch.from_numpy(img).to(device).float()
+    img /= 255.0
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+
+    
+    pred = model(img, augment=False, visualize=False)
+    pred = non_max_suppression(pred, 0.4, 0.45)
+
+    for det in pred:
+        if len(det):
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], frame.shape).round()
+            for *xyxy, conf, cls in det:
+                label = f'{names[int(cls)]} {conf:.2f}'
+                xyxy = [int(x.item()) for x in xyxy]
+                cv2.rectangle(frame, (xyxy[0], xyxy[1]), (xyxy[2], xyxy[3]), (0, 255, 0), 2)
+                cv2.putText(frame, label, (xyxy[0], xyxy[1] - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+    cv2.imshow('YOLOv5 Webcam Detection', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
